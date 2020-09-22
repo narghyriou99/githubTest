6f459584a3b7b6a9e48bf40c6d1eb1570b851559
narghyriou99 <71732319+narghyriou99@users.noreply.github.com>
2020-09-23 00:37:35 +0300

    Update main.cpp
    NEW DELETE

--- main.cpp
+++ main.cpp
@@ -4,11 +4,21 @@ using namespace std;
 
 int main(){
 
+int** a = new int*[10];
+for(int i = 0; i < 10; ++i)
+    a[i] = new int[10];
+
 cout<<"Hello World"<<endl;
 cout<<"NEW UPDATE"<<endl;
 
 cout<<"SECOND UPDATE"<<endl;
 
+    
+
+    for(int i = 0; i < 10; ++i) {
+        delete [] a[i];
+    }
+    delete [] a;
 return 0;
 }
 
